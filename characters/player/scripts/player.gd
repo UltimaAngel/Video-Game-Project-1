f9ca8e20052a9f409698676dadda5590fd31efd2
Lukas-bit <lukasrenschler@gmail.com>
2025-12-01 12:08:22 -0800

    Fit Code to Godot Style Guide

--- characters/player/scripts/player.gd
+++ characters/player/scripts/player.gd
@@ -1,23 +1,20 @@
 extends CharacterBody2D
-
 # NOTES:
-# 
 # 1) A portion of the code in this script (regarding playing idle/walking animations)
 #    is directly inspired by or taken from Michael Games's "Make a 2D Action & Adventure 
 #    RPG in Godot 4" tutorial series on YouTube:
 #    https://www.youtube.com/playlist?list=PLfcCiyd_V9GH8M9xd_QKlyU8jryGcy3Xa
 
-var cardinal_direction : Vector2 = Vector2.DOWN
-var input_direction : Vector2 = Vector2.ZERO
+@export var animation_player : AnimationPlayer
+@export var player_sprite : Sprite2D
 var speed : float = 300.0
-var state : String = "idle"
-
-@onready var animation_player : AnimationPlayer = $AnimationPlayer
-@onready var player_sprite : Sprite2D = $Sprite2D
+var _cardinal_direction : Vector2 = Vector2.DOWN
+var _input_direction : Vector2 = Vector2.ZERO
+var _state : String = "idle"
 
 
 func anim_direction() -> String:
-	match cardinal_direction:
+	match _cardinal_direction:
 		Vector2.LEFT:
 			return "side"
 		Vector2.RIGHT:
@@ -29,45 +26,46 @@ func anim_direction() -> String:
 
 
 func get_input() -> void:
-	input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	_input_direction = Input.get_vector("left", "right", "up", "down")
+	print(_input_direction)
+	velocity = _input_direction * speed
 
 
 func set_direction() -> bool:
 	# Keep same sprite direction if player stops moving
-	if input_direction == Vector2.ZERO:
+	if _input_direction == Vector2.ZERO:
 		return false
 		
-	var new_dir : Vector2 = cardinal_direction
+	var new_dir : Vector2 = _cardinal_direction
 	
-	if abs(input_direction.x) > abs(input_direction.y):
-		new_dir = Vector2.LEFT if input_direction.x < 0 else Vector2.RIGHT
+	if abs(_input_direction.x) > abs(_input_direction.y):
+		new_dir = Vector2.LEFT if _input_direction.x < 0 else Vector2.RIGHT
 	else:
-		new_dir = Vector2.UP if input_direction.y < 0 else Vector2.DOWN
+		new_dir = Vector2.UP if _input_direction.y < 0 else Vector2.DOWN
 	
 	# Note that diagonal movement will not change the sprite direction
-	if new_dir == cardinal_direction:
+	if new_dir == _cardinal_direction:
 		return false
 		
-	cardinal_direction = new_dir
+	_cardinal_direction = new_dir
 	
 	# Scale the player sprite across x axis. Doing it this way instead of setting 
 	# flip_h allows us to also "flip" children of Sprite2D node when the player 
 	# sprite "flips", should we desire to do so in the future.
-	player_sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
+	player_sprite.scale.x = -1 if _cardinal_direction == Vector2.LEFT else 1
 	return true
 
 
 func set_state() -> bool:
-	var new_state : String = "idle" if input_direction == Vector2.ZERO else "walk"
-	if new_state == state:
+	var new_state : String = "idle" if _input_direction == Vector2.ZERO else "walk"
+	if new_state == _state:
 		return false
-	state = new_state
+	_state = new_state
 	return true
 
 
 func update_animation() -> void:
-	animation_player.play(state + "_" + anim_direction())
+	animation_player.play(_state + "_" + anim_direction())
 
 
 func _ready():
@@ -76,7 +74,7 @@ func _ready():
 
 func _physics_process(_delta):
 	get_input()
-	if set_state() || set_direction():
+	if set_state() or set_direction():
 		update_animation()
 	move_and_slide()
 
