d91c45adcd4e7e76edbb514a1fd5399afcfe2699
Lukas-bit <66097115+Lukas-bit@users.noreply.github.com>
2025-12-03 11:30:55 -0800

    Follow GDScript Style Conventions (#25)
    * Fit Code to Godot Style Guide
    
    * Make Player Script Follow GDScript Convention
    
    * Follow GDScript Style Conventions
    
    * Set Up Autoformatter

--- components/health_component/health_component.gd
+++ components/health_component/health_component.gd
@@ -1,14 +1,15 @@
-extends Node2D
 class_name HealthComponent
+extends Node2D
 
+@export var max_health: float = 100.0
+var health: float
 
-@export var MAX_HEALTH : float = 100.0
-var health : float
 
 func _ready():
-	health = MAX_HEALTH
+	health = max_health
 
-# For now, a single attack is enough to kill
-func damage():
+
+func damage() -> void:
+	# For now, a single attack is enough to kill
 	health = 0
 	get_parent().queue_free()
