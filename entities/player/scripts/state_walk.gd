8d564665fe8dee5cfb08a6b5589d18b0abaae5e8
Lukas-bit <66097115+Lukas-bit@users.noreply.github.com>
2026-01-02 11:28:28 -0800

    Lukas/issue 33 bug fix (#34)
    * Fix Game Crash & Player Stuck Walking Bug
    
    Game no longer crashes when attempting to move due; this was due to a variable
    typo. The player also used to get stuck in their walking animation
    when moving in a new direction in the midst of an attack. This was fixed
    by making sure the player only updates the animation in the state
    machine code, rather than in the player script.
    
    * Make prev_dir Variable Private
    
    * Update _prev_dir At End of Each Process Call

--- entities/player/scripts/state_walk.gd
+++ entities/player/scripts/state_walk.gd
@@ -1,6 +1,8 @@
 class_name State_Walk
 extends State
 
+var _prev_dir := Vector2.ZERO
+
 @export var move_speed: float = 300.0
 
 @onready var idle: State = $"../Idle"
@@ -22,6 +24,10 @@ func Process(_delta: float) -> State:
 		return idle
 
 	player.velocity = player.direction * move_speed
+
+	if player.direction != _prev_dir:
+		player.update_animation("walk")
+	_prev_dir = player.direction
 	return null
 
 
