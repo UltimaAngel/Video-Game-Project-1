afaa41b5e70a2aab0770d2ef8946f2ec631fa910
Lukas-bit <66097115+Lukas-bit@users.noreply.github.com>
2026-01-26 13:04:25 -0800

    Fix Sprite Positions and Slime Health (#44)

--- components/health_component/health_component.gd
+++ components/health_component/health_component.gd
@@ -4,17 +4,17 @@ extends Node2D
 signal damaged
 signal destroyed
 
-@export var max_health: float = 100.0
-var health: float
+@export var _max_health: float = 100.0
+var _health: float
 
 
 func _ready():
-	health = max_health
+	_health = _max_health
 
 
 func damage(damage_taken: int) -> void:
-	health -= damage_taken
-	if health > 0:
+	_health -= damage_taken
+	if _health > 0:
 		damaged.emit()
 	else:
 		destroyed.emit()
