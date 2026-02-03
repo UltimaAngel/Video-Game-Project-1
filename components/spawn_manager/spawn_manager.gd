58848c7a90982ae5ce8c981e668014a4ceadf0b9
Lukas-bit <lukasrenschler@gmail.com>
2026-02-02 18:30:53 -0800

    Write Pseudocode For Enemy Spawning

---
+++ components/spawn_manager/spawn_manager.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+# NOTES:
+#
+# var spawn_boundary: Vector2i = get_parent().get_used_rect().size
+# e/s: e enemies spawned per s seconds
+#
+# Things that should be variable:
+# The number of enemies to spawn: e
+# How many seconds until next spawn: s
+# Max number of waves until map cleared
+
+func _ready():
+	# Set timer for s seconds
+	pass
+
+
+func spawn_enemy():
+	# For e amount of times:
+	# Pick random point within spawn_boundary
+	# Repeat until random tile selected is not a "wall" tile
+	# Spawn enemy at this point
+	pass
