2ece8bef534a366499d11bd4587da307ee45275a
Lukas-bit <lukasrenschler@gmail.com>
2026-02-03 21:07:22 -0800

    Create Spawn Manager to Spawn Enemies
    The spawn manager is a new component that will spawn slimes at random
    points within the rectangle of used tiles of the parent TileMapLayer node. It first checks if the selected tile has collision points before selecting it as a spawn point. The number of slimes to spawn at once, the number of waves, and the time between spawns are all variable. Currently, the only bug I'm aware of is that slimes are no longer being drawn according to y-axis ordering; the player is always drawn over the slimes.

--- components/spawn_manager/spawn_manager.gd
+++ components/spawn_manager/spawn_manager.gd
@@ -1,23 +1,42 @@
-extends Node
+extends Node2D
 
-# NOTES:
-#
-# var spawn_boundary: Vector2i = get_parent().get_used_rect().size
-# e/s: e enemies spawned per s seconds
-#
-# Things that should be variable:
-# The number of enemies to spawn: e
-# How many seconds until next spawn: s
+@export var enemy: PackedScene
+# The number of enemies to spawn at a time
+@export var enemy_num: int = 1
 # Max number of waves until map cleared
+@export var max_waves: int = 3
+# How many seconds until next spawn
+@export var min_spawn_break: float = 5
+
+var _map: TileMapLayer
+var _map_used_rect_size := Vector2i(0, 0)
+var _timer := Timer.new()
+var _wave_num: int = 0
+
 
 func _ready():
-	# Set timer for s seconds
-	pass
+	get_parent().ready.connect(_on_map_ready)
+	_timer.timeout.connect(_on_timer_timeout)
+	add_child(_timer)
 
 
-func spawn_enemy():
-	# For e amount of times:
-	# Pick random point within spawn_boundary
-	# Repeat until random tile selected is not a "wall" tile
-	# Spawn enemy at this point
-	pass
+func _on_map_ready():
+	_map = get_parent()
+	_map_used_rect_size = _map.get_used_rect().size
+	_timer.start(min_spawn_break)
+
+
+func _on_timer_timeout():
+	for e in enemy_num:
+		var rand_pos: Vector2i
+		while (1):
+			rand_pos = Vector2i(randi_range(0, _map_used_rect_size.x - 1), randi_range(0, _map_used_rect_size.y - 1))
+			if _map.get_cell_tile_data(rand_pos).get_collision_polygons_count(0) == 0:
+				break
+		var slime: Entity = enemy.instantiate()
+		print(16 * Vector2(rand_pos))
+		slime.global_position = 16 * Vector2(rand_pos)
+		add_child(slime)
+	_wave_num += 1
+	if _wave_num == max_waves:
+		_timer.stop()
