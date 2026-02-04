dc6854ccce43b69307d237210d8f604c287049fd
Lukas-bit <lukasrenschler@gmail.com>
2026-02-04 14:47:35 -0800

    Create To-Do in SpawnManager Script

--- components/spawn_manager/spawn_manager.gd
+++ components/spawn_manager/spawn_manager.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+# To-Do: Allow for variable number of preloaded PackedScenes
 @export var enemy: PackedScene
 # The number of enemies to spawn at a time
 @export var enemy_num: int = 1
@@ -31,11 +32,16 @@ func _on_timer_timeout():
 		var rand_pos: Vector2i
 		while (1):
 			rand_pos = Vector2i(randi_range(0, _map_used_rect_size.x - 1), randi_range(0, _map_used_rect_size.y - 1))
+			# To-Do: Make sure that cell tile data even exists
+			#        Also, can we check every physics layer that exists for this TileMapLayer?...
 			if _map.get_cell_tile_data(rand_pos).get_collision_polygons_count(0) == 0:
 				break
 		var slime: Entity = enemy.instantiate()
+		# To-Do: The number 16 below should really be stored and used as a constant.
+		#        Or better yet, get the length of the tile_size property from the TileMapLayer node (_map)
 		slime.global_position = 16 * Vector2(rand_pos)
 		add_child(slime)
 	_wave_num += 1
 	if _wave_num == max_waves:
 		_timer.stop()
+		# To-Do: Emit a signal that the final wave has been completed
