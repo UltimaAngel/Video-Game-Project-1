57e3d5bca5a5ef588b283955d2a9cca8388aaf96
Lukas-bit <66097115+Lukas-bit@users.noreply.github.com>
2026-02-06 11:43:16 -0800

    Create Spawn Manager (#53)
    * Write Pseudocode For Enemy Spawning
    
    * Change SpawnManager Node Type to Node
    
    * Make SpawnManager Script Extend Node
    
    * Create Spawn Manager to Spawn Enemies
    
    The spawn manager is a new component that will spawn slimes at random
    points within the rectangle of used tiles of the parent TileMapLayer node. It first checks if the selected tile has collision points before selecting it as a spawn point. The number of slimes to spawn at once, the number of waves, and the time between spawns are all variable. Currently, the only bug I'm aware of is that slimes are no longer being drawn according to y-axis ordering; the player is always drawn over the slimes.
    
    * Fix Y-Sorting For Slimes
    
    Enabling y-sort for the map TileMapLayer node and SpawnManager, as well
    as disabling z_as_relative for the SpawnManager (z index should no
    longer be relative to parent's z-index, so SpawnManager and its child
    nodes should have a z-index of 0), fixes the y-sorting for slimes so
    that they are no longer always drawn behind the player.
    
    * Remove Debug Prints & Enemies
    
    * Create To-Do in SpawnManager Script
    
    * Make Enemy Spawns Random & Ensure TileData Exists
    
    The SpawnManager can now preload an array of packed scenes for future
    enemy types. This allows for multiple types of enemies to spawn during a
    wave. There is also a new conditional for checking if the randomly
    selected tile exists. This accounts for any future, non-rectangular maps
    where the used tile rectangle might encompass tiles that don't actually
    exist. There is also a new signal that will emit when the final wave has
    been cleared. Finally, wave_num has been marked as public in case any
    future UI wants to indicate the current wave number.

---
+++ components/spawn_manager/spawn_manager.gd
@@ -0,0 +1,53 @@
+extends Node2D
+
+signal final_wave_cleared
+
+# Allows for a variable number of enemy types to spawn
+@export var enemy_array: Array[PackedScene]
+# The number of enemies to spawn at a time
+@export var enemy_num: int = 1
+# Max number of waves until map cleared
+@export var max_waves: int = 3
+# How many seconds until next spawn
+@export var min_spawn_break: float = 5
+
+var wave_num: int = 0
+var _map: TileMapLayer
+var _map_used_rect_size := Vector2i(0, 0)
+var _tile_length: int = 16
+var _timer := Timer.new()
+
+
+func _ready():
+	get_parent().ready.connect(_on_map_ready)
+	_timer.timeout.connect(_on_timer_timeout)
+	add_child(_timer)
+
+
+func _on_map_ready():
+	_map = get_parent()
+	_map_used_rect_size = _map.get_used_rect().size
+	_tile_length = _map.tile_set.tile_size.x
+	_timer.start(min_spawn_break)
+
+
+func _on_timer_timeout():
+	for e in enemy_num:
+		var rand_pos: Vector2i
+		# Keep selecting a random tile within rectangular map boundary to spawn enemy on until
+		# selected tile exists and does not have a collision layer
+		while (1):
+			rand_pos = Vector2i(
+				randi_range(0, _map_used_rect_size.x - 1),
+				randi_range(0, _map_used_rect_size.y - 1),
+			)
+			var rand_tile_data: TileData = _map.get_cell_tile_data(rand_pos)
+			if rand_tile_data and rand_tile_data.get_collision_polygons_count(0) == 0:
+				break
+		var rand_enemy: Entity = enemy_array.pick_random().instantiate()
+		rand_enemy.global_position = _tile_length * Vector2(rand_pos)
+		add_child(rand_enemy)
+	wave_num += 1
+	if wave_num == max_waves:
+		_timer.stop()
+		final_wave_cleared.emit()
