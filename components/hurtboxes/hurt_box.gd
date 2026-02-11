aa33886506e78ac14969c4958a00d1e6ef76a7cf
Lukas-bit <66097115+Lukas-bit@users.noreply.github.com>
2026-02-11 11:39:45 -0800

    Fix Slime Knockback Direction Bug (#60)
    * Fix Slime Knockback Direction Bug
    
    The solution I came up with is to create classifications of hurt boxes. In this case, there's a new classification of hurt boxes that defines them
    as "Melee" hurt boxes. For all melee hurtboxes, a signal will emit on
    collision that a melee attack occured. The attacking, parent entity class will
    then set their hurt box's direction to be their cardinal direction.
    This way, anything hurt by a melee attack will be sent into the
    direction that the attacking entity is facing.
    
    * Remove Unused Variables and Virtual Methods
    
    * Remove Duplicate Hurt Box Reference in Player Script

--- components/hurtboxes/hurt_box.gd
+++ components/hurtboxes/hurt_box.gd
@@ -1,19 +1,20 @@
 class_name HurtBox
 extends Area2D
 
+signal melee_attack
+
 @export var damage: int = 1
+@export_enum("Melee") var attack_type: int
+
+var direction := Vector2.ZERO
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	area_entered.connect(AreaEntered)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func AreaEntered(a: Area2D) -> void:
+	if attack_type == 0:
+		melee_attack.emit()
 	if a is HitBox:
 		a.TakeDamage(self)
--- entities/enemies/slime/scripts/enemy_state_destroy.gd
+++ entities/enemies/slime/scripts/enemy_state_destroy.gd
@@ -3,7 +3,6 @@ extends State
 @export var knockback_speed: float = 400.0
 @export var decelerate_speed: float = 10.0
 
-var _damage_position: Vector2
 var _direction := Vector2.ZERO
 
 
@@ -14,7 +13,6 @@ func init() -> void:
 func enter() -> void:
 	entity.is_invulnerable = true
 	entity.hurt_box.monitoring = false
-	_direction = entity.global_position.direction_to(_damage_position)
 	entity.direction = _direction
 	entity.velocity = _direction * -knockback_speed
 	entity.update_animation(anim_name)
@@ -27,7 +25,10 @@ func process(delta: float) -> State:
 
 
 func _on_destroyed(hurt_box: HurtBox) -> void:
-	_damage_position = hurt_box.global_position
+	if hurt_box.attack_type == 0:
+		_direction = hurt_box.direction * -1
+	else:
+		_direction = entity.global_position.direction_to(hurt_box.global_position)
 	get_parent().change_state(self)
 
 
